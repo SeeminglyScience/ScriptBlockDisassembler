242b5691aea79254b9ddf214ab421000d05f0589
Patrick Meinecke <SeeminglyScience@users.noreply.github.com>
2021-11-16 14:13:38 -0500

    Fix github actions (#1)

--- src/ScriptBlockDisassembler/PSExpressionTranslation.cs
+++ src/ScriptBlockDisassembler/PSExpressionTranslation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq.Expressions;
 using AgileObjects.ReadableExpressions;
 using AgileObjects.ReadableExpressions.Translations;
--- src/ScriptBlockDisassembler/RecursiveReducer.cs
+++ src/ScriptBlockDisassembler/RecursiveReducer.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Collections.Concurrent;
